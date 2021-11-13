ed1db173ea6cf16d666913a1e7fa732dd6c02bbb
Fazle Bari <fazlebarisn@gmail.com>
2021-11-13 12:33:53 +0600

    add comment blade

---
+++ resources/views/livewire/comments.blade.php
@@ -0,0 +1,26 @@
+<div class="w-1/2 m-auto">
+    <h1 class="text-3xl mt-4">Comments</h1>
+    @error('newComment')
+        <span class="text-red-600">nnnnn</span>
+    @enderror
+    <form class="my-4 flex" wire:submit.prevent="addComment">
+
+        <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="What's in your mind.">
+
+        <div class="py-2">
+            <button class="p-2 bg-blue-500 w-20 rounded shadow text-white" type="submit">Add</button>
+        </div>
+
+    </form>
+        <div class="rounded border shadow p-3 my-2">
+
+            <div class="flex justify-start my-2">
+                <p class="font-bold text-lg">kkkkkkk</p>
+                <p class="mx-3 py-1 text-xs text-gray-500 font-semibold">nnnnnnnn</p>
+            </div>
+
+            <p class="text-gray-800">nnnnn</p>
+
+        </div>  
+
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>LiveWire</title>
     @livewireStyles
 </head>
+
 <body>
-    <livewire:counter />
+    <livewire:comments />
     @livewireScripts
 </body>
+
 </html>
